add48e05b37ef5148b7c9aa83cddf5f2f1529e41
Shadman Saif Anonno <38778826+Shadman77@users.noreply.github.com>
2019-06-18 21:53:52 +0600

    Add files via upload

---
+++ js/loading_screen.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', (event) => {
+    document.getElementById('loading_screen').style.visibility = 'hidden';
+    //document.getElementById('whole_document').style.display = 'block';
+    document.body.style.overflowY = "scroll";
+});
